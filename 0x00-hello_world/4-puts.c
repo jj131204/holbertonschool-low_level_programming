eddd1b2705cc9cd8c6467749ccf27be79861a313
jj131204 <jj131204@gmail.com>
2021-02-03 22:53:40 +0000

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
+#include <stdio.h>
+#include <string.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-#include<stdio.h>
-#include <string.h>
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return (0);
+char str1[52];
+strcpy(str1, ""Programming is like building a multilingual puzzle");
+puts(str1);
+
+return (0);
 }
