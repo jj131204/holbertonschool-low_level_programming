42afd5ae967bf8f377bae9bbbf98c35a7121f874
jj131204 <jj131204@gmail.com>
2021-02-03 22:41:50 +0000

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@
 #include<stdio.h>
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n")
+	puts("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
