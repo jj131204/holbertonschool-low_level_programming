96f45244b2694bfdb9f6e2e0d7ac0b2994f80ef4
jj131204 <jj131204@gmail.com>
2021-02-08 20:13:30 +0000

    C

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include"holberton.h"
+#include <stdio.h> 
+/**
+ * main - check the code for Holberton School students.
+ *
+ * print_alphabet - Prints the alphabet in lowercase.
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+int i;
+	for (i = 'a' ; i <= 'z' ; i++)
+{
+	
+	_putchar(i);
+}
+	_putchar (0);
+	return (0);
+}
