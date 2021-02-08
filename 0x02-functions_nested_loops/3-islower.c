f349b488b1de9a4cee13a8df78b2b2a467f60a40
jj131204 <jj131204@gmail.com>
2021-02-08 22:52:47 +0000

    C

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _islower - Check if the entry is in lower.
+ * @c: This is the entry
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (c != c)
+{
+	return (0);
+}
+	else
+{
+	return (1);
+}
+
+}
