2828bfc5bdc2920b5ee38610b0236cecafff8257
jj131204 <jj131204@gmail.com>
2021-02-09 18:49:15 +0000

    C

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * add - Sum between two numbers
+ *
+ * @a : number one
+ *
+ * @e : number two
+ * Return: The result
+ */
+
+int add(int a, int e)
+{
+int i;
+	i = a + e;
+	_putchar(i);
+	return (i);
+}
