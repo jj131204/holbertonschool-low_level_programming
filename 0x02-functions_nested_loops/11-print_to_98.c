a0739854264cfd3f89f08c438ff05f676a2cd23f
jj131204 <jj131204@gmail.com>
2021-02-09 19:21:19 +0000

    C

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,8 +1,10 @@
 #include "holberton.h"
 #include <stdio.h>
 
-/*
-*
+/**
+* print_to_98- prints to 98
+* @n: number
+* Return:n
 *
 */
 
@@ -11,13 +13,13 @@ void print_to_98(int n)
 	if (n <= 98)
 {
 	for (; n < 98 ; n++)
-		printf("%d, ",n);
+		printf("%d, ", n);
 		printf("%d\n", n);
 }
 	else
 {
-        for (; n > 98 ; n--)
-        	printf("%d, ",n);
+	for (; n > 98 ; n--)
+		printf("%d, ", n);
 		printf("%d\n", n);
 }
 }
