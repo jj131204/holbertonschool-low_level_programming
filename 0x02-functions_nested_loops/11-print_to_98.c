0943744c96e6830b4a70857276e81a1022052a50
jj131204 <jj131204@gmail.com>
2021-02-09 19:16:47 +0000

    C

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/*
+*
+*
+*/
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+{
+	for (; n < 98 ; n++)
+		printf("%d, ",n);
+		printf("%d\n", n);
+}
+	else
+{
+        for (; n > 98 ; n--)
+        	printf("%d, ",n);
+		printf("%d\n", n);
+}
+}
