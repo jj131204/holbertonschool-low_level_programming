8f46723bcee4615cb6413ed5458e1bc1093c27c3
jj131204 <jj131204@gmail.com>
2021-02-09 15:45:23 +0000

    header

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,7 @@
+void print_alphabet(void); /*punto 1*/ 
+int _putchar(char c); /*putchar*/
+void print_alphabet_x10(void); /*punto 2*/
+int _islower(int c); /*punto 3 */
+int _isalpha(int c); /*punto 4 */
+int print_sign(int n); /*punto 5*/
+int _abs(int); /*punto 6*/
