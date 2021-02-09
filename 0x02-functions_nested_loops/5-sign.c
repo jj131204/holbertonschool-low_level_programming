93751fe123360755259735f05e54c3dbbfd5142a
jj131204 <jj131204@gmail.com>
2021-02-09 01:20:11 +0000

    C

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,20 +8,20 @@
 int print_sign(int n)
 {
 int i = n % 10;
-	if (i >= 1 )
+	if (i >= 1)
 {
 	_putchar('+');
-	return(1);
+	return (1);
 }
-	else if (i == 0 )
+	else if (i == 0)
 {
 	_putchar('-');
-	return(0);
+	return (0);
 }
 	else
 {
 	_putchar('-');
-	return(-1);
+	return (-1);
 }
 }
 
