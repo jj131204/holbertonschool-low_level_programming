427038d1102db79196038542798cbc1a8db6631e
jj131204 <jj131204@gmail.com>
2021-02-09 01:13:20 +0000

    C

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _islower - Check if the entry is in lower.
+ * @c: This is the entry
+ *
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+        if (c >= 93 && c <= 122)
+{
+        return (1);
+}
+	if (c >= 65 && c <= 90)
+{
+	return (1);
+}
+        else
+{
+        return (0);
+}
+
+}
