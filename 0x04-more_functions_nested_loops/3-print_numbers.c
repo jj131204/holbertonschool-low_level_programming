335ec519c09f618e672bb7328e5b155e4acd0526
jj131204 <jj131204@gmail.com>
2021-02-11 19:30:18 +0000

    C

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,16 @@
 #include "holberton.h"
 
 /**
- *  print_numbers - imprime numeros del 1 al 9
- *
- *
- *
- * Return: return las digit
- **/
-
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
+ */
 void print_numbers(void)
 {
-int i;
-	for (i = 48 ; i <= 57 ; i++)
-		putchar(i);
-		putchar('\n');
+	int ch;
+
+	for (ch = 48; ch < 58; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
 }
