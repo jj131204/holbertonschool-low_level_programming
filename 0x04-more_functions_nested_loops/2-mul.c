05c1518a07228bc45d35b3edbad2c1da2237bbfd
jj131204 <jj131204@gmail.com>
2021-02-11 18:48:19 +0000

    C

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - return 1 if c  between 0 & 9
+ *
+ * @c: This is the entry
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+int c;
+	c = a * b;
+	return (c);
+}           
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -5,5 +5,6 @@
 
 int _isupper(int c); /*punto 0*/
 int _isdigit(int c); /*punto 1*/
+int mul(int a, int b); /*punto 2*/
 
 #endif /* HOLBERTON_H */
