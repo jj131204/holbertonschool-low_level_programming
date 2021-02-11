e261bbd752d47965b4180ecdbcada5f3caf4911c
jj131204 <jj131204@gmail.com>
2021-02-11 20:30:30 +0000

    H

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,6 +1,10 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
+
+#endif
