7f4cace4c8c24ee1059b8e29ccedc700b1856b5f
jj131204 <jj131204@gmail.com>
2021-02-12 00:26:54 +0000

    C

---
+++ 0x04-more_functions_nested_loops/2-assembler.c
@@ -0,0 +1,16 @@
+nclude "holberton.h"
+
+/**
+ * mul - multiplca int a con int b
+ *
+ *
+ * @b: This is the entry
+ * @a: This is the entry
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+int c;
+	c = a * b;
+	return (c);
+}
