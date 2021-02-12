fc36750264cd1eb7375147fd60fab31e520a45d6
jj131204 <jj131204@gmail.com>
2021-02-12 03:00:31 +0000

    C

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,12 +11,14 @@ void print_square(int size)
 {
 	int i, c;
 
-	for (i = 0; i < size ; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (c = 0; c <= size ; c++)
+		for (c = 0; c <= size; c++)
 		{
 			_putchar(35);
 		}
+	if (i != size - 1)
 		_putchar('\n');
 	}
+	_putchar('\n');
 }
