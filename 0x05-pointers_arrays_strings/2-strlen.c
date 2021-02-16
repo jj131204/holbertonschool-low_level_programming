929d8d850e59559112909dbe02979b9961e010cf
jj131204 <jj131204@gmail.com>
2021-02-16 17:18:14 +0000

    prunteros

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,20 @@
 #include "holberton.h"
 
+/**
+ * _strlen - This function
+ *
+ * @s: this is my
+ *
+ * Return: the las
+ */
 int _strlen(char *s)
 {
 int longitud = 0;
-	while(*s != '\0')
+	while (*s != '\0')
 	{
 		longitud++;
 		s = s + 1;
 	}
-	return(longitud);
+	return (longitud);
 
 }
