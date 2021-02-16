9e4bc60fe0dc52c5157e5a7850a957036f55ff9c
jj131204 <jj131204@gmail.com>
2021-02-16 03:08:47 +0000

    punteros

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,18 +2,17 @@
 #include <stdio.h>
 
 /**
- * _puts : function that prints a string, followed by a new line, to stdout
- *@str pointer
+ * _puts : puntero
+ *@str : pointer
  * Return: Always 0. (sucess)
  */
 
 void _puts(char *str)
 {
-	while(*str != '\0')
+	while (*str != '\0')
 	{
 		_putchar(*str);
 		str = str + 1;
 	}
 	_putchar('\n');
-
 }
