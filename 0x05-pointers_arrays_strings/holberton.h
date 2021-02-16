3f6b716ab48cd7644b9664ae64154907b47dcb58
jj131204 <jj131204@gmail.com>
2021-02-16 17:09:30 +0000

    header

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,10 @@
+#ifndef _HOLBERTONH
+#define _HOLBERTONH
+
+int _putchar(char c);  /*_putchar*/
+void reset_to_98(int *n);  /*punto 0*/
+void swap_int(int *a, int *b); /*punto 1*/
+int _strlen(char *s); /*punto 2*/
+void _puts(char *str); /*punto 3*/
+
+#endif
