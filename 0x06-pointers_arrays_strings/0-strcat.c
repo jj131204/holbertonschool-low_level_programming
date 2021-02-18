5dd857e80b35e7146a16ec71fdd51f5c3ec424c2
jj131204 <jj131204@gmail.com>
2021-02-18 23:27:18 +0000

    pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,19 @@
 #include "holberton.h"
 
 /**
-*
-*
-*/
+ * _strcat -  concatenates two strings.
+ * @src: array with the string source
+ * @dest: pointer where the text is append
+ *
+ * Return: Always 0.
+ */
 
 char *_strcat(char *dest, char *src)
 {
 int a, b;
 	for (a = 0 ; dest[a] != '\0' ; a++)
 	{
-	
+
 	}
 	for (b = 0 ; src[b] != '\0' ; b++)
 	{
