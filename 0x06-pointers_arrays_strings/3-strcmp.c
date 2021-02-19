03efcf5dda33ef93ef0e7ee84e392e606bfeb7d7
jj131204 <jj131204@gmail.com>
2021-02-19 02:43:30 +0000

    pointers

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,22 +1,20 @@
 #include "holberton.h"
 /**
- * _strcmp - compare two strings.
- * @s1: string 1
- * @s2: string 2
- *
- * Return: Always 0.
- */
-
+  * _strcmp - compares two strings
+  * @s1: string 1
+  * @s2: string 2
+  * Return: integer
+**/
 int _strcmp(char *s1, char *s2)
 {
-int a, b;
-	while (s1[a] == s2[a] && s2[a] != '\0')
+int a= 0;
+int b = 0;
+
+	while (s1[a] == s2[a] && s1[a] != '\0')
 	{
 		a++;
 	}
 	if (s1[a] != s2[a])
-	{
-		b = s1[a] - s1[a];
-	}
+		b = s1[a] - s2[a];
 	return (b);
 }
