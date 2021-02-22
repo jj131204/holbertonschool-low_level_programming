f191df755d8079cf8de96eea27517f3ec364f5c5
jj131204 <jj131204@gmail.com>
2021-02-22 19:02:10 +0000

    C

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _memcpy - This is my function copy the number the character to S
+ * @dest: This is my entry and return
+ * @src: This is the character to copy
+ * @n: This is the number of Bytes
+ *
+ * Return: This is my result and Return dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int a;
+	for (a = 0 ; a < n ; a++)
+	{
+		dest = src;
+	}
+	return (dest);
+}
