a763e91e6f39a5f368684803a263f5b269cc9733
jj131204 <jj131204@gmail.com>
2021-02-23 01:18:16 +0000

    C

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -7,5 +7,6 @@ char *_memcpy(char *dest, char *src, unsigned int n); /*punto1*/
 char *_strchr(char *s, char c); /*punto2*/
 unsigned int _strspn(char *s, char *accept); /*punto3*/
 char *_strpbrk(char *s, char *accept); /*punto4*/
+char *_strstr(char *haystack, char *needle); /*punto5*/
 
 #endif
