4db4bff6ec1ccd62ff83b431926d324c60f165a1
jj131204 <jj131204@gmail.com>
2021-02-23 02:04:07 +0000

    C

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,7 +1,6 @@
 #ifndef _HOLBERTONH
 #define _HOLBERTONH
 
-#include<stdio.h> /*libreria  estandar*/
 char *_memset(char *s, char b, unsigned int n); /*punto0*/
 char *_memcpy(char *dest, char *src, unsigned int n); /*punto1*/
 char *_strchr(char *s, char c); /*punto2*/
