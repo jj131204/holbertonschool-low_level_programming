b587dfc69f4ba81600e2c9dd8cdef8615fa6336f
jj131204 <jj131204@gmail.com>
2021-02-24 22:55:09 +0000

    C

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -5,5 +5,6 @@ int _putchar(char c);/*puthar*/
 void _puts_recursion(char *s); /*punto0*/
 void _print_rev_recursion(char *s); /*punto1*/
 int _strlen_recursion(char *s); /*punto2*/
+int factorial(int n); /*punto3*/
 
 #endif
