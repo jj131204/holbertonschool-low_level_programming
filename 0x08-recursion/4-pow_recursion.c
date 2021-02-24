c1f07c0303dcbe6d789f6942d4887f05190516f1
jj131204 <jj131204@gmail.com>
2021-02-24 23:13:36 +0000

    C

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,11 @@
+#include  "holberton.h"
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return (x * _pow_recursion(x,y--));
+	
+}
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -6,5 +6,6 @@ void _puts_recursion(char *s); /*punto0*/
 void _print_rev_recursion(char *s); /*punto1*/
 int _strlen_recursion(char *s); /*punto2*/
 int factorial(int n); /*punto3*/
+int _pow_recursion(int x, int y); /*punto4*/
 
 #endif
