a830724f66cca07dc5f1bafae115252a39202e3c
jj131204 <jj131204@gmail.com>
2021-02-24 22:56:52 +0000

    C

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,5 +1,12 @@
 #include "holberton.h"
 
+/**
+ * factorial - get factorial of a number
+ * @n: is a given number
+ * Return: factorial or -1 if n is lees than 0 or
+ * 1 if n is 0.
+ */
+
 int factorial(int n)
 {
 	if (n == 0)
