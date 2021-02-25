3c4e4543e14b200758ddf4e97fec14f321ff01bc
jj131204 <jj131204@gmail.com>
2021-02-25 04:49:14 +0000

    C

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,60 +1,57 @@
 #include "holberton.h"
+
 /**
-  * _puts_recursion - checks the length of a string
-  * @s: is the string
-  * Return: return the leng of the string
-**/
-int _puts_recursion(char s)
+ * _strlen - returns the length of a string in a recursive manner.
+ * @s: the string to be print.
+ *
+ * Return: the length of the string.
+ */
+int _strlen(char *s)
 {
-	if (s == '\0')
-		return (0);
-	return (1 + _puts_recursion(s + 1));
+	if (*s != '\0')
+		return (1 + _strlen(++s));
+	return (0);
 }
+
 /**
- *_print_rev_recursion - helper function to verify if a string is palindromic
+ * _palindrome - helper function to verify if a string is palindromic
  *               or not.
  * @s: the string to be operated upon.
- * @p: the length of the string.
- * @q: the half of the string.
+ * @len: the length of the string.
+ * @stop: the half of the string.
  *
  * Return: 1 if palindrome, 0 otherwise.
  */
-
-int _print_rev_recursion(char s, int p, int q)
+int _palindrome(char *s, int len, int stop)
 {
-	if (s != *(s + p))
-	{
+	if (*s != *(s + len))
 		return (0);
-	}
-	if (p >= q)
-	{
-		return (_print_rev_recursion(s + 1, p - 2, q));
-}
+	if (len >= stop)
+		return (_palindrome(s + 1, len - 2, stop));
 	return (1);
 }
+
 /**
- *is_palindrome - returns if a string is palindromic.
+ * is_palindrome - returns if a string is palindromic.
  * @s: the string to be operated.
  *
  * Return: the helper function _prime(n, 5)
  */
-
 int is_palindrome(char *s)
-
 {
-	int p;
-	int q;
+	int len;
+	int stop;
 
-	p = _puts_recursion(s);
-	if (p % 2 == 0)
-	{
-		q = ((p / 2) - 1);
-	}
+	len = _strlen(s);
+
+	if (len % 2 == 0)
+		stop = (len / 2) - 1;
 	else
-	{
-		q = p / 2;
-	}
-	if (p == 0)
+		stop = len / 2;
+
+	if (len == 0)
 		return (1);
-	return (_print_rev_recursion(s, p - 1, q));
+
+	return (_palindrome(s, len - 1, stop));
 }
+
