2e2ca5952b5636c590cf4ba53702994ed5d70998
jj131204 <jj131204@gmail.com>
2021-02-28 17:58:27 +0000

    argv - argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *main - main function
+ *@argc: size of array of command
+ *@argv: array of command
+ *Return:  0
+ */
+
+int main(int argc, char *argv[])
+{
+argv[argc] = 0;
+	printf("%d\n", argc);
+	return (0);
+}
