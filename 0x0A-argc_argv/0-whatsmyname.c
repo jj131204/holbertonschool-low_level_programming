bd04df72d167644d8165315e4da2e21f02cbbb3b
jj131204 <jj131204@gmail.com>
2021-02-28 17:26:09 +0000

    argv -  argc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,16 @@
 #include "holberton.h"
 #include <stdio.h>
 
-int main (int argc, char * argv [])
+/**
+*main - main function
+ *@argc: size of array of command
+ *@argv: array of command
+ *Return:  0
+*/
+
+int main(int argc, char *argv[])
 {
 argc = 0;
-	printf ("%s\n",argc[argv]);
+	printf("%s\n", argc[argv]);
 	return (0);
 }
