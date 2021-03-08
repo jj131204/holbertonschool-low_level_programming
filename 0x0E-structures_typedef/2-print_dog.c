e7382642925145e48931cb44ef8078c11fbdc3aa
jj131204 <jj131204@gmail.com>
2021-03-08 23:38:23 +0000

    0x0E

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,6 +11,7 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: ");
 		d->name == NULL ? printf("(nil)\n") : printf("%s\n", d->name);
+		 printf("age: ");
 		d->age == NULL ? printf("(nil)\n") : printf("Age: %f\n", d->age);
 		printf("Owner: ");
 		d->owner == NULL ? printf("(nil)\n") : printf("%s\n", d->owner);
