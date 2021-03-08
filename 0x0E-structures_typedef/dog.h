d9db188bf151f5126c07344e2c74619d638bcdb0
jj131204 <jj131204@gmail.com>
2021-03-08 22:02:31 +0000

    0x0E

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - Breve descripción
+* @name: Primer miembro
+* @age: Segundo miembro
+* @owner: tercer miembro
+*
+* Descripción: Descripción más larga
+*/
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+} dog;
+#endif
