2291c9714ac67b6e7025650b697c8f21de9eab0a
jj131204 <jj131204@gmail.com>
2021-03-09 01:28:38 +0000

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,49 +1,106 @@
 #include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
 
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+
 /**
- * new_dog - Function that creates a new dog.
- * @name: type pointer char name.
- * @age: type pointer float age
- * @owner: type pointer char owner
- * Return: 0.
+ * new_dog - Creates a new dog.
+ *
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Owner of the dog.
+ *
+ * Return: If the fuction fails, returns (0).
+ *         Else, returns a new dog.
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *django;
-	char *i, *j;
-	int x, y, z;
+	dog_t *my_new_dog;
 
-	django = malloc(sizeof(struct dog));
-	if (django == NULL)
-		return (NULL);
+	my_new_dog = malloc(sizeof(dog_t));
 
-	for (x = 0; *(name + x) != '\0'; x++)
-		;
-	for (y = 0; *(owner + y) != '\0'; y++)
-		;
-	i = malloc(sizeof(char) * x + 1);
-	if (i == NULL)
+	if (my_new_dog == 0)
+		return (0);
+
+	if (name == 0)
+		my_new_dog->name = 0;
+
+	else
 	{
-		free(django);
-		return (NULL);
+		my_new_dog->name = (char *) malloc(_strlen(name) + 1);
+
+		if (my_new_dog->name == 0)
+		{
+			free(my_new_dog);
+			return (0);
+		}
+
+		my_new_dog->name = _strcpy(my_new_dog->name, name);
 	}
-	j = malloc(sizeof(char) * y + 1);
-	if (j == NULL)
+
+	if (owner == 0)
+		my_new_dog->owner = 0;
+
+	else
 	{
-		free(i);
-		free(django);
-		return (NULL);
+		my_new_dog->owner = (char *) malloc(_strlen(owner) + 1);
+
+		if (my_new_dog->owner == 0)
+		{
+			free(my_new_dog->owner);
+			free(my_new_dog);
+			return (0);
+		}
+
+		my_new_dog->owner = _strcpy(my_new_dog->owner, owner);
 	}
-	for (z = 0; z <= x; z++)
-		*(i + z) = *(name + z);
-	for (z = 0; z <= y; z++)
-		*(j + z) = *(owner + z);
 
-	django->name = i;
-	django->age = age;
-	django->owner = j;
+	my_new_dog->age = age;
 
-	return (django);
+	return (my_new_dog);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: String pointer
+ *
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (*s != '\0')
+	{
+		c++;
+		s++;
+	}
+	return (c);
+}
+
+/**
+ * _strcpy - copies the string pointed to by src,
+ *           including the terminating null byte (\0),
+ *           to the buffer pointed to by dest.
+ *
+ * @dest: Destiny
+ * @src: Source
+ * Return: Source Value
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
 }
