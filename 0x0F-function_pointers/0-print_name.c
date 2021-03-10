c44a78acc3b3d22bcd89b8c6290675a4aca09aec
jj131204 <jj131204@gmail.com>
2021-03-10 22:47:51 +0000

    0x0F

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,18 +1,19 @@
-#include "function_pointers.h"
 #include <stdio.h>
+#include "function_pointers.h"
 /**
-*print_name - it's passed parameter through function pointer
+ * print_name - it's passed parameter through function pointer
  * @name: name of the person
  * @f: name function pointer
-*
-*/
-
+ *
+ * Return: Nothing.
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (!f)
-		return;		
+		return;
 
 	if (!name)
 		return;
-	f(name);
+
+	f(name); /* (*f)(name) is the same and both is dereference */
 }
