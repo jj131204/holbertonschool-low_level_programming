cb0e0f6017b6494bf2ac9c22842b455c99e435a0
jj131204 <jj131204@gmail.com>
2021-03-11 03:19:20 +0000

    0x0F

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,13 +18,7 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
-	if ((*argv[2] != '+' &&
-	     *argv[2] != '-' &&
-	     *argv[2] != '*' &&
-	     *argv[2] != '/' &&
-	     *argv[2] != '%') ||
-	    argv[2][1] != 0
-		)
+	if (get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
