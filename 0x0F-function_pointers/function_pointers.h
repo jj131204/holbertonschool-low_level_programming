bae52b1889aef33e4cc851c3841a27bc40198a9d
jj131204 <jj131204@gmail.com>
2021-03-11 02:22:16 +0000

    0x0F

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef FUN_POINTER_H
 #define FUN_POINTER_H
+#include <stdio.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
