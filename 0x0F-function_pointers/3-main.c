e502f37742c3b4217341b80c7572eddc2ef920bf
jj131204 <jj131204@gmail.com>
2021-03-11 03:34:22 +0000

    0x0F

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,23 +1,17 @@
-#include <stdio.h>
 #include "3-calc.h"
-#include <stdlib.h>
 /**
- * main - Enter point
- * @argc: Argument to main counter
- * @argv: Argument to main array to pointer to char
- *
- * Return: Always 0.
- */
-
-int main(int argc, char **argv)
+  * main - receives two integers and a operator
+  * @argc: number of arguments
+  * @argv: array of arguments
+  * Return: 0 if success
+**/
+int main(int argc, char *argv[])
 {
-
 	if (argc != 4)
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		exit(98);
 	}
-
 	if ((*argv[2] != '+' &&
 	     *argv[2] != '-' &&
 	     *argv[2] != '*' &&
@@ -25,8 +19,11 @@ int main(int argc, char **argv)
 	     *argv[2] != '%') ||
 	    argv[2][1] != 0
 		)
-
-	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == 0)
 	{
 		printf("Error\n");
 		exit(100);
