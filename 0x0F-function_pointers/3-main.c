609cf8768b57e6eaf0f91aad7b500e23dbf1abe1
jj131204 <jj131204@gmail.com>
2021-03-11 02:45:44 +0000

    0x0F

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,9 +18,15 @@ int respuesta;
 		exit(98);
 	}
 
-	if (argv[2] == NULL)
+	if ((*argv[2] != '+' &&
+	     *argv[2] != '-' &&
+	     *argv[2] != '*' &&
+	     *argv[2] != '/' &&
+	     *argv[2] != '%') ||
+	    argv[2][1] != 0
+		)
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		exit(99);
 	}
 	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
