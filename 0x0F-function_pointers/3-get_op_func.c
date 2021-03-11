286fd5398f06b40b3f25602a887037200fe8829d
jj131204 <jj131204@gmail.com>
2021-03-11 02:54:51 +0000

    0x0F

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,11 +22,8 @@ int (*get_op_func(char *s))(int, int)
 
 	while ((ops[i]).op != NULL)
 	{
-		if (*(ops[i]).op == *s)
-		{
+		if (*s == *ops[i].op)
 			return (ops[i].f);
-			s = (s + 1);
-		}
 
 	i++;
 	}
