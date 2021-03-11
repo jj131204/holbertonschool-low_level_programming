f444d784c929c9cd9b54659f7f9943f76d1df221
jj131204 <jj131204@gmail.com>
2021-03-11 03:04:14 +0000

    0x0F

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,7 +11,7 @@
 
 int main(int argc, char **argv)
 {
-int respuesta;
+
 	if (argc != 4)
 	{
 		printf("ERROR\n");
@@ -37,4 +37,3 @@ int respuesta;
 	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
-
