c052d1f2e7e520a988336d42f8fa27ce9d3e1c13
jj131204 <jj131204@gmail.com>
2021-03-19 21:42:06 +0000

    second

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * print_list - Prints all the elements of a list.
+ *
+ * @h: The number of nodes.
+ *
+ * Return: The number of nodes.
+ */
+
 size_t list_len(const list_t *h)
 {
 int  n = 0;
