fb62cd0c00ff8d5b349f05465b22bc316f64b6d0
jj131204 <jj131204@gmail.com>
2021-03-19 21:20:34 +0000

    fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -22,9 +22,10 @@ size_t print_list(const list_t *h)
 		{
 			printf("[0] (nil)\n");
 		}
-
-		printf("[%d] %s\n", h->len, h->str);
-
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
 		h = h->next;
 		n++;
 	}
