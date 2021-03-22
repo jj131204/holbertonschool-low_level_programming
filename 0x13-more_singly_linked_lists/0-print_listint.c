de26e70ae8d9f893d8281086f407372684103dea
jj131204 <jj131204@gmail.com>
2021-03-22 21:33:14 +0000

    betty errors

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,10 +1,13 @@
 #include "lists.h"
 
 /**
+ * print_listint - Prints all the elements of a list.
  *
+ * @h: The number of nodes.
  *
- *
+ * Return: The number of nodes.
  */
+
 
 size_t print_listint(const listint_t *h)
 {
