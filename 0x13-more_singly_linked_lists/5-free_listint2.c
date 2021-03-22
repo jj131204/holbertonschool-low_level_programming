4f81ff0cc71ac1d1108494ebc6c6982d93d343b7
jj131204 <jj131204@gmail.com>
2021-03-22 22:55:33 +0000

    fix

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,25 +1,20 @@
 #include "lists.h"
 /**
- * free_listint - function that frees a list_t list.
+ * free_listint2 - function that frees a list_t list.
  * @head: Pointed to head of a list
  *
  * Return: Void
- *
  */
 
-
-void free_listint(listint_t *head)
+void free_listint2(listint_t **head)
 {
-	listint_t *new_node = head;
+	listint_t *new_node = *head;
 
 	listint_t *next = NULL;
 
 	for (; new_node != NULL; new_node = next)
 	{
 		next = new_node->next;
-		/*free(&new_node->n);*/
 		free(new_node);
 	}
-
-
 }
