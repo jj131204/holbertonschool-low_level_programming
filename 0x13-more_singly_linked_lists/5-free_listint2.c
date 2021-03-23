63175eb91f1cb77a6f8d29f0263ce90f1d7a9915
jj131204 <jj131204@gmail.com>
2021-03-23 14:56:14 +0000

    fix

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,11 @@
 void free_listint2(listint_t **head)
 {
 
-	listint_t *next = NULL;
+	listint_t *next;
 
 	while (*head)
 	{
-		next = (*head)->next; /* *(head) desreferencia head */
+		next = (*head)->next;
 		free(*head);
 		(*head) = next;
 	}
