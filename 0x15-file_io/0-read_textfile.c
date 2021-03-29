867f2377ef2b31a9c77e3cf710adbee3205e380d
jj131204 <jj131204@gmail.com>
2021-03-29 20:54:54 +0000

    prueba

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - function that reads a text file and prints.
+ * @filename : file
+ * @letters : is the number of letters
+ * Return: the actual number of letters, read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	int i = 0, j = 0;
+	char *buffer;
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(letters));
+
+	i = read(fd, buffer, letters);
+
+	if (i == -1)
+		return (0);
+
+	close(fd); /*close files*/
+
+	j = write(, buffer, i); /*write file*/
+
+	if (j == -1)
+		return (0);
+
+	return (j);
+
+}
