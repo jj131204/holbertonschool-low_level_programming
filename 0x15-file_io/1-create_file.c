a06838ce7be98fa8990782b158ad5626c9c6889a
jj131204 <jj131204@gmail.com>
2021-03-29 23:50:23 +0000

    fix errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,9 +20,11 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	while (text_content[a])
-		a++;
-
+	if (text_content)
+	{ 	
+		while (text_content[a])
+			a++;
+	}
 	write(fd, text_content, a);
 
 	close(fd);
