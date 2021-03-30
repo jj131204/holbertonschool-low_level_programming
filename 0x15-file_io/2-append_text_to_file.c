b483392ba933a98b3e5099d08fffe8fc56ee5020
jj131204 <jj131204@gmail.com>
2021-03-30 00:36:31 +0000

    fix2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,6 +15,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
+	if (!text_content)
+		return (1);
+
 	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (text_content)
