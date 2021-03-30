8088407bdad48eaa6ba57fb465f97d7790de4145
jj131204 <jj131204@gmail.com>
2021-03-30 02:12:07 +0000

    prueba2

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int fd, fd2;	
+	int fd, fd2;
 
 	int len, format;
 	char buffer[1024];
@@ -11,7 +11,7 @@ int main (int argc, char *argv[])
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit (97);	
+		exit(97);
 	}
 
 	fd = open(argv[1], O_RDONLY);
@@ -33,17 +33,26 @@ int main (int argc, char *argv[])
 		len = read(fd, buffer, 1024);
 		format = write(fd2, buffer, len);
 	}
-	if (len < 0)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-		argv[1]), exit(98);
-	if (format < 0)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
-		argv[2]), exit(99);
-	if (close(fd) < 0)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
+	if (len == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (format == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 
-	if (close(fd2) < 0)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2), exit(100);
-	
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+	if (close(fd2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
 	return (0);
 }
