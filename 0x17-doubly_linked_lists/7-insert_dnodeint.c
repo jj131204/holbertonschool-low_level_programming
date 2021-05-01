5b38684867e67c9803492926386fe25274a28ebf
jj131204 <jj131204@gmail.com>
2021-05-01 23:02:20 +0000

    fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,14 +18,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (h == NULL)
 		return (NULL);
-
 	head = *h;
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-
 	new_node->n = n;
-
 	if (idx == 0)
 	{
 		if (head->next)
@@ -43,8 +40,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			head = head->next;
 			count++;
 		}
-		else
+		else if (!head->next)
+		{
+			head = head->next;
 			return (NULL);
+		}
 	}
 	if (head->next)
 		head->next->prev = new_node;
