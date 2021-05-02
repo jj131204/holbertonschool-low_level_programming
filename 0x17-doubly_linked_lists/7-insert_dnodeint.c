80fcea2814ca044fdbf2529401fe8cb832fc7e29
jj131204 <jj131204@gmail.com>
2021-05-02 00:53:43 +0000

    fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -31,7 +31,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (count2 < idx)
 		return (NULL);
 	new_node->n = n;
-	if (idx - 1 == 0)
+	if (idx == 0)
 	{
 		if (head->next)
 		{
