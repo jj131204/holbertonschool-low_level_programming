c8e5a259c8d73858ac507da0991a8344f61d916f
jj131204 <jj131204@gmail.com>
2021-05-29 01:26:53 +0000

    fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,17 +2,18 @@
 
 /**
  * hash_table_set - adds an element to the hash table
+ *
  * @ht: the hash table you want to add or update the key/value to
+ *
  * @key: the key. key can not be an empty string
+ *
  * @value: the value associated with the key
- * value must be duplicated
- * value can be an empty string
  *
  * Return: 1 if it succeeded, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new;
+	hash_node_t *new = NULL, *aux = NULL;
 	unsigned int index;
 
 	if (ht == NULL || key == NULL)
@@ -20,16 +21,18 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index((const unsigned char *)key, ht->size);
 
-	while (ht->array[index])
+	aux = ht->array[index];
+
+	while (aux)
 	{
-		if (strcmp(ht->array[index]->key, key) == 0)
+		if (strcmp(aux->key, key) == 0)
 		{
-			free(ht->array[index]->value);
-			ht->array[index]->value = strdup(value);
+			free(aux->value);
+			aux->value = strdup(value);
 
 			return (1);
 		}
-		ht->array[index] = ht->array[index]->next;
+		aux = aux->next;
 	}
 
 	new = malloc(sizeof(hash_node_t));
